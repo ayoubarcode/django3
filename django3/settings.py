3ee4656a489abfb55f20a6c84dd73f696220036a
ayoub ar <ayoubarahmat9@gmail.com>
2020-03-30 21:00:50 +0100

    fix static problem

--- django3/settings.py
+++ django3/settings.py
@@ -127,14 +127,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, "static_file"),
+    os.path.join(BASE_DIR, 'django3/static')
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 #smptp configuration
 
@@ -143,4 +144,4 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='soufianmrabti3@gmail.com'
-EMAIL_HOST_PASSWORD='password'
+EMAIL_HOST_PASSWORD='gd#@ay9581'
